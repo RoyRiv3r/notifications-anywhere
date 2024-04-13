f96f9a233d89e2cbeed06d93199b44ece5ca11d2
RoyRiver <41067116+RoyRiv3r@users.noreply.github.com>
2024-04-13 10:31:58 +0200

    Changing output name NotificationAnywhere

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -lib:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" -r:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Microsoft.VisualBasic.dll" -target:winexe -out:"NotificationPositioner.exe" "NotificationPositioner.cs"
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -lib:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" -r:"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Microsoft.VisualBasic.dll" -target:winexe -out:"NotificationAnywhere.exe" "NotificationAnywhere.cs"
 
 PAUSE
 
